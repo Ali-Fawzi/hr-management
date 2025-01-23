dbfec47097e8a1ab5a61353d964d308b0ba8a396
Ali <alialdarraji095@gmail.com>
2025-01-23 20:56:28 +0300

    Users, Roles, Permisions management logic

--- database/migrations/2025_01_23_141553_create_permission_tables.php
+++ database/migrations/2025_01_23_141553_create_permission_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -26,7 +26,7 @@ return new class extends Migration
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');      
+            $table->string('name');
             $table->string('guard_name');
             $table->timestamps();
 
